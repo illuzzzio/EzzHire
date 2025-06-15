71c72cf9e2009b27225eeea87b0d153184163cb2
Your Name <you@example.com>
2025-06-15 17:57:51 +0530

    vapi

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,12 +1,14 @@
 import Agent from "@/components/Agent";
+import { getCurrentUser } from "@/lib/actions/auth.action";
 
 import React from 'react'
 
-const page = () => {
+const page = async  () => {
+  const user = await getCurrentUser();
   return (
    <>
    <h3>Interview Generator</h3>
-<Agent userName="You" />
+<Agent userName={user?.name ?? ""} userId={user?.id} type="generate" />
    </>
   )
 }
